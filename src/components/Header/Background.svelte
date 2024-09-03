484d0d58bafb3712c32301cc8921738be328fa7f
Rūdolfs Ošiņš <rudolfs@osins.org>
2024-09-03 14:04:25 +0200

    Update dependencies

--- src/components/Header/Background.svelte
+++ src/components/Header/Background.svelte
@@ -1,3 +1,6 @@
+<script lang="ts">
+</script>
+
 <style>
   .container {
     height: 3rem;
--- src/views/Repos.svelte
+++ src/views/Repos.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import Header from "@app/components/Header.svelte";
 </script>
 
