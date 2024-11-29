b7b71ccb11f50b5862501a5a11d8450726092b90
Sebastian Martinez <me@sebastinez.dev>
2024-11-29 13:21:07 +0100

    Move ` AppState` to `radicle-types`

--- crates/radicle-types/src/lib.rs
+++ crates/radicle-types/src/lib.rs
@@ -1,5 +1,32 @@
+use traits::auth::Auth;
+use traits::cobs::Cobs;
+use traits::issue::{Issues, IssuesMut};
+use traits::patch::{Patches, PatchesMut};
+use traits::repo::Repo;
+use traits::thread::Thread;
+use traits::Profile;
+
 pub mod cobs;
 pub mod config;
 pub mod error;
 pub mod repo;
+pub mod test;
 pub mod traits;
+
+pub struct AppState {
+    pub profile: radicle::Profile,
+}
+
+impl Auth for AppState {}
+impl Repo for AppState {}
+impl Thread for AppState {}
+impl Cobs for AppState {}
+impl Issues for AppState {}
+impl IssuesMut for AppState {}
+impl Patches for AppState {}
+impl PatchesMut for AppState {}
+impl Profile for AppState {
+    fn profile(&self) -> radicle::Profile {
+        self.profile.clone()
+    }
+}
