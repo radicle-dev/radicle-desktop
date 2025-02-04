67836558caebed4c6bf1bcc6e86a26c08781f4c4
Rūdolfs Ošiņš <rudolfs@osins.org>
2025-02-04 14:32:48 -0600

    Remove old Upstream license header

--- src/lib/mutexExecutor.ts
+++ src/lib/mutexExecutor.ts
@@ -1,9 +1,3 @@
-// Copyright © 2021 The Radicle Upstream Contributors
-//
-// This file is part of radicle-upstream, distributed under the GPLv3
-// with Radicle Linking Exception. For full terms see the included
-// LICENSE file.
-
 //@ts-expect-error the typescript bindings are out of date.
 import * as Bacon from "baconjs";
 
