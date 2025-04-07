61a2d5fb6cd5af6e6c7408b0c47d21f5fd61c68a
Sebastian Martinez <me@sebastinez.dev>
2025-04-07 14:12:23 +0200

    Add patch checkout popover

---
+++ src/components/Command.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Clipboard from "@app/components/Clipboard.svelte";
+  import Border from "./Border.svelte";
+
+  interface Props {
+    command: string;
+    styleWidth: string;
+  }
+
+  const { command, styleWidth }: Props = $props();
+
+  let clipboard: Clipboard;
+</script>
+
+<style>
+  .cmd {
+    color: var(--color-foreground-dim);
+  }
+  .cmd:hover {
+    color: var(--color-foreground-contrast);
+  }
+</style>
+
+<div class="cmd txt-monospace" style:width={styleWidth}>
+  <Border
+    hoverable
+    onclick={() => clipboard.copy()}
+    styleBackgroundColor="var(--color-background-float)"
+    styleCursor="pointer"
+    styleJustifyContent="space-between"
+    stylePadding="0.25rem 0.5rem"
+    {styleWidth}
+    variant="ghost">
+    $ {command}
+    <Clipboard bind:this={clipboard} text={command} />
+  </Border>
+</div>
