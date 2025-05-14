a010c04ab12372fc80ccf2df60ff13c867d83e5c
Rūdolfs Ošiņš <rudolfs@osins.org>
2025-05-14 10:24:53 +0200

    Extract issue state filter button

--- src/components/IssueSecondColumn.svelte
+++ src/components/IssueSecondColumn.svelte
@@ -1,40 +1,35 @@
 <script lang="ts">
   import type { Issue } from "@bindings/cob/issue/Issue";
-  import type { RepoInfo } from "@bindings/repo/RepoInfo";
   import type { IssueStatus } from "@app/views/repo/router";
+  import type { RepoInfo } from "@bindings/repo/RepoInfo";
 
-  import capitalize from "lodash/capitalize";
   import fuzzysort from "fuzzysort";
 
   import * as router from "@app/lib/router";
-  import { issueStatusColor, modifierKey } from "@app/lib/utils";
+  import { modifierKey } from "@app/lib/utils";
 
   import Border from "./Border.svelte";
-  import DropdownList from "./DropdownList.svelte";
-  import DropdownListItem from "./DropdownListItem.svelte";
   import Icon from "./Icon.svelte";
+  import IssueStateFilterButton from "./IssueStateFilterButton.svelte";
   import IssueTeaser from "@app/components/IssueTeaser.svelte";
   import Link from "./Link.svelte";
   import NakedButton from "./NakedButton.svelte";
   import OutlineButton from "./OutlineButton.svelte";
-  import Popover, { closeFocused } from "./Popover.svelte";
   import TextInput from "./TextInput.svelte";
 
   const activeRouteStore = router.activeRouteStore;
 
   interface Props {
+    changeFilter: (status: IssueStatus) => void;
+    issues: Issue[];
     repo: RepoInfo;
     selectedIssueId?: string;
-    issues: Issue[];
     status: IssueStatus;
     title: string;
-    changeFilter: (status: IssueStatus) => void;
   }
 
-  /* eslint-disable prefer-const */
-  let { repo, selectedIssueId, issues, status, title, changeFilter }: Props =
+  const { changeFilter, issues, repo, selectedIssueId, status, title }: Props =
     $props();
-  /* eslint-enable prefer-const */
 
   const project = $derived(repo.payloads["xyz.radicle.project"]!);
 
@@ -80,24 +75,6 @@
     gap: 2px;
   }
 </style>
-
-{#snippet icons(status: IssueStatus)}
-  <div
-    class="icon"
-    style:color={status === "all" ? undefined : issueStatusColor[status]}>
-    <Icon name={status === "closed" ? "issue-closed" : "issue"} />
-  </div>
-{/snippet}
-
-{#snippet counters(status: IssueStatus)}
-  <div style:margin-left="auto" style:padding-left="0.25rem">
-    {#if status === "all"}
-      {project.meta.issues.open + project.meta.issues.closed}
-    {:else}
-      {project.meta.issues[status]}
-    {/if}
-  </div>
-{/snippet}
 
 <div class="container">
   <div
@@ -157,37 +134,10 @@
 
 {#if showFilters}
   <div class="global-flex" style:margin="1rem 0">
-    <Popover popoverPositionLeft="0" popoverPositionTop="3rem">
-      {#snippet toggle(onclick)}
-        <OutlineButton variant="ghost" {onclick} styleHeight="2.5rem">
-          {@render icons(status)}
-          {capitalize(status)}
-          {@render counters(status)}
-          <Icon name="chevron-down" />
-        </OutlineButton>
-      {/snippet}
-
-      {#snippet popover()}
-        <Border variant="ghost">
-          <DropdownList items={["all", "open", "closed"] as IssueStatus[]}>
-            {#snippet item(state)}
-              <DropdownListItem
-                styleGap="0.5rem"
-                styleMinHeight="2.5rem"
-                selected={status === state}
-                onclick={() => {
-                  changeFilter(state);
-                  closeFocused();
-                }}>
-                {@render icons(state)}
-                {capitalize(state)}
-                {@render counters(state)}
-              </DropdownListItem>
-            {/snippet}
-          </DropdownList>
-        </Border>
-      {/snippet}
-    </Popover>
+    <IssueStateFilterButton
+      {status}
+      counters={project.meta.issues}
+      {changeFilter} />
     <TextInput
       onSubmit={async () => {
         if (searchResults.length === 1) {
