c3aa74f4c96855eeb8c74e6aa15d82a403f00f38
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-06-03 10:06:15 +0200

    Polish arch packaging scripts

--- arch/generate-srcinfo.sh
+++ arch/generate-srcinfo.sh
@@ -2,10 +2,10 @@
 
 exec docker run \
     --user 1000 \
-    --rm --volume "$(pwd):/workdir:ro" \
+    --rm --volume "$(pwd)/radicle-desktop:/workdir:ro" \
     --env BUILDDIR=/tmp \
     --env PKGDEST=/tmp \
     --env SRCDEST=/tmp \
     --workdir /workdir \
-    archlinux:base-devel makepkg --printsrcinfo > .SRCINFO
+    archlinux:base-devel makepkg --printsrcinfo > radicle-desktop/.SRCINFO
 
