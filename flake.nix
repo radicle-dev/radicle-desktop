2c91b1e08312b7db2d78557c72dca73d0dfd8242
Fintan Halpenny <fintan.halpenny@gmail.com>
2026-05-19 15:35:53 +0100

    nix: Update nixpkgs 25.11
    Update the nixpkgs URL to point to the latest version: `25.11`.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Radicle Desktop App";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -19,49 +19,57 @@
     rust-overlay,
     heartwood,
     ...
-  }:
-    (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (import rust-overlay)
-        ];
-      };
-    in {
+  }: (flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        (import rust-overlay)
+      ];
+    };
+  in {
+    checks = {
+      radicle-desktop = self.packages.${system}.radicle-desktop.overrideAttrs {doCheck = true;};
+    };
 
-      checks = {
-        radicle-desktop = self.packages.${system}.radicle-desktop.overrideAttrs ({ doCheck = true; });
-      };
-
-      devShells.default = pkgs.mkShell {
-        name = "radicle-desktop-env";
-        inputsFrom = [ self.checks.${system}.radicle-desktop ];
-        nativeBuildInputs = with pkgs; [
-          cargo-watch
-          cargo-nextest
-          ripgrep
-          rust-analyzer
-        ];
-        env = self.checks.${system}.radicle-desktop.env // {
+    devShells.default = pkgs.mkShell {
+      name = "radicle-desktop-env";
+      inputsFrom = [self.checks.${system}.radicle-desktop];
+      nativeBuildInputs = with pkgs; [
+        cargo-watch
+        cargo-nextest
+        ripgrep
+        rust-analyzer
+      ];
+      env =
+        self.checks.${system}.radicle-desktop.env
+        // {
         };
+    };
+
+    packages = {
+      default = self.packages.${system}.radicle-desktop;
+      twemoji-assets = pkgs.fetchFromGitHub {
+        owner = "twitter";
+        repo = "twemoji";
+        rev = "v14.0.2";
+        hash = "sha256-YoOnZ5uVukzi/6bLi22Y8U5TpplPzB7ji42l+/ys5xI=";
       };
 
-      packages = {
-        default = self.packages.${system}.radicle-desktop;
-        twemoji-assets = pkgs.fetchFromGitHub {
-          owner = "twitter";
-          repo = "twemoji";
-          rev = "v14.0.2";
-          hash = "sha256-YoOnZ5uVukzi/6bLi22Y8U5TpplPzB7ji42l+/ys5xI=";
-        };
-
-        radicle-desktop = pkgs.callPackage ./nix/radicle-desktop.nix {
+      radicle-desktop =
+        pkgs.callPackage ./nix/radicle-desktop.nix {
           inherit heartwood;
           inherit (self.packages.${system}) twemoji-assets;
         }
-        // (if self ? rev || self ? dirtyRev then {
-          GIT_HEAD = if self ? rev then self.rev else self.dirtyRev;
-        } else {});
-      };
-    }));
+        // (
+          if self ? rev || self ? dirtyRev
+          then {
+            GIT_HEAD =
+              if self ? rev
+              then self.rev
+              else self.dirtyRev;
+          }
+          else {}
+        );
+    };
+  }));
 }
