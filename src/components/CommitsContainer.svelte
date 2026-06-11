2b2cc6ec0d76d7c549470f2a6214f38be9415a90
Brandon Oxendine <brandon.oxendine@gmail.com>
2026-06-11 19:32:25 +0200

    Make the commits list non-collapsible

--- src/components/CommitsContainer.svelte
+++ src/components/CommitsContainer.svelte
@@ -9,8 +9,6 @@
   }
 
   const { leftHeader, children }: Props = $props();
-
-  let expanded = $state(true);
 </script>
 
 <style>
@@ -20,97 +18,25 @@
     z-index: 1;
     display: flex;
     align-items: center;
+    gap: 0.5rem;
     height: 2.5rem;
     padding: 0 0.5rem;
     font: var(--txt-body-m-regular);
-    cursor: pointer;
     background-color: var(--color-surface-canvas);
-  }
-  .header.expanded {
     border-bottom: 1px solid var(--color-border-subtle);
   }
-  .header:hover,
-  .header:focus-visible {
-    background-color: var(--color-surface-subtle);
-  }
-  .left {
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-  }
-  .icon-stack,
-  .icon-expanded {
+  .icon {
     display: grid;
     width: 1rem;
     height: 1rem;
     place-items: center;
   }
-  .icon-default,
-  .icon-hover {
-    grid-area: 1 / 1;
-    transition:
-      opacity 150ms ease,
-      transform 150ms ease;
-  }
-  .icon-hover {
-    opacity: 0;
-    transform: rotate(-90deg);
-  }
-  .header:hover .icon-default,
-  .header:focus-visible .icon-default {
-    opacity: 0;
-    transform: rotate(90deg);
-  }
-  .header:hover .icon-hover,
-  .header:focus-visible .icon-hover {
-    opacity: 1;
-    transform: rotate(0);
-  }
-  .collapsible {
-    display: grid;
-    grid-template-rows: 0fr;
-    transition: grid-template-rows 180ms ease-out;
-    overflow: hidden;
-    width: 100%;
-  }
-  .collapsible.open {
-    grid-template-rows: 1fr;
-  }
-  .collapsible-inner {
-    overflow: hidden;
-    min-height: 0;
-  }
 </style>
 
-<div
-  style:display="flex"
-  style:align-items="flex-start"
-  style:flex-direction="column"
-  style:width="100%">
-  <!-- svelte-ignore a11y_click_events_have_key_events -->
-  <div
-    class="header"
-    class:expanded
-    role="button"
-    tabindex="0"
-    onclick={() => (expanded = !expanded)}
-    style:width="100%">
-    <div class="left">
-      {#if expanded}
-        <span class="icon-expanded"><Icon name="chevron-down" /></span>
-      {:else}
-        <span class="icon-stack">
-          <span class="icon-default"><Icon name="commit" /></span>
-          <span class="icon-hover"><Icon name="chevron-down" /></span>
-        </span>
-      {/if}
-      {@render leftHeader()}
-    </div>
+<div style:display="flex" style:flex-direction="column" style:width="100%">
+  <div class="header">
+    <span class="icon"><Icon name="commit" /></span>
+    {@render leftHeader()}
   </div>
-
-  <div class="collapsible" class:open={expanded}>
-    <div class="collapsible-inner">
-      {@render children()}
-    </div>
-  </div>
+  {@render children()}
 </div>
